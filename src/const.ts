3aa306c69fcde6a389a2fa75553213c873e47719
Carezapato <pxreturn2@gmail.com>
2023-02-16 09:11:42 -0300

    mejoras graficas celular

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,5 @@
 export const version = "0.3.1";
 export const appName = "Zoomix";
 
-export const urlBase = 'http://localhost:8080';
+//export const urlBase = 'http://localhost:8080';
+export const urlBase = 'http://192.168.1.85:8080';
